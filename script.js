ed387bc2d3d6169177bca4f203f2b81ced5fc3f3
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-14 11:44:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 //your code here
 // Implementing myCall
 Function.prototype.myCall = function(thisContext, ...args) {
-  
+    const originalFunc = this;
+    thisContext = thisContext || window;
+    thisContext.fn = originalFunc;
+    const result = thisContext.fn(...args);
+    delete thisContext.fn;
+    return result;
   };
   
   // Implementing myApply
   Function.prototype.myApply = function(thisContext, args) {
-    
-  
+    const originalFunc = this;
+    thisContext = thisContext || window;
+    thisContext.fn = originalFunc;
+    const result = thisContext.fn(...args);
+    delete thisContext.fn;
+    return result;
   };
   
   // Implementing myBind
   Function.prototype.myBind = function(thisContext, ...args) {
-    
+    const originalFunc = this;
+    thisContext = thisContext || window;
+    return function(...bindArgs) {
+      const result = originalFunc.myCall(thisContext, ...args, ...bindArgs);
+      return result;
+    };
   };
   
   module.exports = Function.prototype;
